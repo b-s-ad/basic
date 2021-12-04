f777c852a07a57f05ffe0f92905c2a802d12bbb7
b-s-ad <ahmedshehbazb@gmail.com>
2021-12-04 10:47:47 +0530

    Create Program.cs
    ODD / EVEN Without %

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace oddeven
+{
+    class Program
+    {
+    public static void Main(string[] args)  
+    {
+        int n;
+        //console.Console.Clear();
+        
+        Console.WriteLine("\nEnter a number to check : ","\n");
+
+
+            n = Convert.ToInt32(Console.ReadLine());
+
+            
+            if ((n / 2) * 2 == n)
+            {
+                Console.WriteLine("\tit is even number : " +n);
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine("\tit is odd number : " +n);
+                Console.ReadKey();
+            }
+        }
+    }
+}
