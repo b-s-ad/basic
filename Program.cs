b794b9d2cfb0e492e3ed626b783d83464cab7a43
b-s-ad <ahmedshehbazb@gmail.com>
2021-12-04 10:50:58 +0530

    Prime Number
    Prime Number  1 to Any

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using static System.Console;
+
+
+namespace prime1toAny
+
+{
+    class Program
+    {
+         public static void Main()
+        {
+            int i = 2;
+            Clear();
+            WriteLine("\nFind the 1 to 100(Any) prime number using while loop : \n");
+            int k=int.Parse(Console.ReadLine());
+            while (i < k)
+            {
+                bool count = false;
+                for (int j = 2; j < i; j++)
+                {
+                    if (i % j == 0)
+                    {
+                        count = true;
+                        break;
+                    }
+                }
+                if (count == false)
+                {
+                    Write(i + " ");
+                }
+                i++;
+            }
+            ReadLine();
+        }
+    }
+}
