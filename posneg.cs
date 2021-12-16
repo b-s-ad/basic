d709bd667d8b7db27cc410d8f859b99e7f8826a8
b-s-ad <ahmedshehbazb@gmail.com>
2021-12-17 00:12:30 +0530

    -- / ++
    -- / ++

---
+++ posneg.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace posneg
+{
+     class Program
+        {
+            static void Main(string[] args)
+            {
+            Console.Clear();   
+            int num;          
+            Console.Write("Enter an integer : ");
+            //num= Convert.ToInt32(Console.ReadLine());
+            num=-43;
+
+            if (num >= 0) // greater than equal to 0
+            Console.WriteLine("{0} is a positive number.\n",num);
+            else
+            Console.WriteLine("{0} is a negative number. \n", num);
+            }
+        }
+}
