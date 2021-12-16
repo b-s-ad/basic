a6f60df84612987920f9006c43fd5303a1499de5
b-s-ad <ahmedshehbazb@gmail.com>
2021-12-17 00:27:08 +0530

    Perfect_Number

---
+++ perfect_n.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using static System.Console;
+using  static System.ConsoleColor;
+
+namespace perfect_n    /*that number is equal to that number. For example 6 is perfect number since divisor of 6 are 1, 2 and 3.*/
+                       /*Sum of its divisor is 1 + 2+ 3 = 6*/
+{
+    class Program
+    {
+        static void Main(string[] args)
+            {  
+                    Clear();
+                    int number,sum=0,n;
+                    WriteLine("Enter the Number : ");
+                    n = int.Parse(ReadLine());
+                     
+                    for (int i = 1; i < n;i++)
+                    {
+                    if (n % i == 0)
+                    {
+                    sum=sum + i;
+                    }
+                    }
+                    if (sum == n)
+
+                    {
+                    Console.ForegroundColor=Blue;
+                    WriteLine(n+" is a perfect number");
+                    ReadLine();
+                    }
+                    else
+                    {
+                    Console.ForegroundColor=Magenta;
+                    WriteLine(n+" is not a perfect number");
+                    ReadLine();
+                    }
+            }
+    }
+}
