5b7c105d907c1996b58c7751b2da7cd368826c4e
b-s-ad <ahmedshehbazb@gmail.com>
2021-12-17 00:06:18 +0530

    PrimeorNOT
    PrimeorNOT

---
+++ PrimeorNOT.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using static System.Console;
+
+namespace prime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Clear();
+            int n,Isprime;
+            System.Console.WriteLine("Enter the Number : ");
+            n=int.Parse(ReadLine());//1,3,7,11,13,17,19
+            Isprime=0;
+            for (int i = 2; i < n/2; i++)
+            {
+                if (n%i==0)
+                {
+                    Isprime=1;
+                    break;
+                }
+            }
+            if (Isprime==0)
+            {
+            WriteLine("This is Prime Number");                
+            }
+            else
+            {
+            WriteLine("This is NOT Prime Number");    
+            }
+
+        }
+    }
+}
