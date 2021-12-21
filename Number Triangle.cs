0716e6d23b1b6db9c998cbce5cb6c2fd3d76b0a6
b-s-ad <ahmedshehbazb@gmail.com>
2021-12-21 15:56:59 +0530

    Patterns

---
+++ Number Triangle.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ANumber_Triangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            
+           int  i,j,k,l,n;           
+       Console.Write("Enter the Range=");    
+       n= int.Parse(Console.ReadLine());     
+       for(i=1; i<=n; i++)      
+       {          
+        for(j=1; j<=n-i; j++)      
+        {      
+         Console.Write(" ");      
+        }      
+        for(k=1;k<=i;k++)      
+        {      
+         Console.Write(k);      
+        }      
+        for(l=i-1;l>=1;l--)      
+        {      
+        Console.Write(l);      
+        }      
+        Console.Write("\n");      
+       }       
+   }  
+  }  
+}
+
+/* 
+C# Program to print Number Triangle
+
+Like alphabet triangle, we can write the C# program to print the number triangle. 
+The number triangle can be printed in different ways. */
---
+++ pat_Star.cs
@@ -0,0 +1,55 @@
+﻿using System;
+					
+public class DiamondTriangle
+{
+	public static void Main()
+	{
+            Console.Clear();
+		     Console.WriteLine("Program for displaying pattern of *.");
+            Console.Write("Enter the maximum number of *: (Top to Middle line) : ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+		  //loop to print first half or equilateral triangle
+             for (int i = 1; i <= n; i++)
+            {
+                //for loop to add space
+				for (int j = 1; j <= (n - i); j++)
+				{
+					Console.Write(" ");
+				}
+                //for loop to print *, for each line 
+				//k < i *2
+				//so for example, if i =1, it will print star for 1 times, as loop will work  until k < 2, that is 1 time
+				// when i = 2, condition will be k < 4, means it will print star for 3 times
+				// and so on.
+				for (int k = 1; k < i * 2; k++)
+				{
+					Console.Write("*");
+				}
+                Console.WriteLine();
+            } System.Console.WriteLine("\n\n");
+
+/* *********************************************************************************************** */		  
+           //loop to print inverted equilateral triangle
+            for (int i = n - 1; i >= 1; i--)
+            {
+                 //for loop to add space
+				for (int j = 1; j <= (n - i); j++)
+				{
+					Console.Write(" ");
+				}
+                //for loop to print *, for each line 
+				//k < i *2
+				//so for example, if i =1, it will print star for 1 times, as loop will work  until k < 2, that is 1 time
+				// when i = 2, condition will be k < 4, means it will print star for 3 times
+				// and so on.
+				for (int k = 1; k < i * 2; k++)
+				{
+					Console.Write("*");
+				}
+                Console.WriteLine();
+            }
+ 
+            Console.WriteLine();
+	}
+}
