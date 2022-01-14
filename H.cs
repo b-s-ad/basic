57fb1afbfa58fb3a54dfb68b789eeec89ad09beb
b-s-ad <89061373+b-s-ad@users.noreply.github.com>
2022-01-13 17:58:01 -0800

    H

---
+++ H.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Alpha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            System.Console.WriteLine("\nAlphabet_pattern : H\n");
+            for (int i = 1; i <= 5; i++)
+            {
+                for (int j = 1; j <= 5; j++)
+                {
+
+                    string H = (i == 3 || j == 1 || j == 5) ? "*" : " ";
+                    System.Console.Write(H);
+                }
+                System.Console.WriteLine();
+            }
+            /* ***************************************************** */
+        }
+    }
+}
+
