a6a120ba912afc37c4dc254f46bf713a6efc7b29
b-s-ad <89061373+b-s-ad@users.noreply.github.com>
2022-01-13 18:23:51 -0800

    L

---
+++ L.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Alpha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            System.Console.WriteLine("\nAlphabet_pattern : L \n");
+            for (int i = 1; i <= 5; i++)
+            {
+                for (int j = 1; j <= 5; j++)
+                {
+                    string L = (i == 5 || j == 1) ? "*" : " ";
+
+                    System.Console.Write(L);
+                }
+                System.Console.WriteLine();
+            }
+            /* ***************************************************** */
+        }
+    }
+}
+
