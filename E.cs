46ba822548d1c6401600cd8583cbd827ac039e0e
b-s-ad <89061373+b-s-ad@users.noreply.github.com>
2022-01-13 17:28:41 -0800

    E

---
+++ E.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Alpha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            System.Console.WriteLine("\nAlphabet_pattern : E\n");
+            for (int i = 1; i <= 5; i++)
+            {
+                for (int j = 1; j <= 5; j++)
+                {
+                    string E = (i == 1 || i == 5 || i == 3 || j == 1 || j == 0) ? "*" : " ";
+                    System.Console.Write(E);
+                }
+                System.Console.WriteLine();
+            }
+            /* ***************************************************** */
+        }
+    }
+}
