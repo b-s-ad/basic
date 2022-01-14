39aaa6d8788e7635d398cf22a2ff67ccdbfed41e
b-s-ad <89061373+b-s-ad@users.noreply.github.com>
2022-01-13 18:05:59 -0800

    T

---
+++ T.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Alpha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            System.Console.WriteLine("\nAlphabet_pattern : T\n");
+            for (int i = 1; i <= 4; i++)
+            {
+                for (int j = 1; j <= 9; j++)
+                {
+                    string T = (i == 1 || j == 5 ||i==1) ? "*" : " ";
+                    System.Console.Write(T);
+                }
+                System.Console.WriteLine();
+            }
+            /* ***************************************************** */
+        }
+    }
+}
+
