7687b70c116961d7208dc0d137136b478022d66a
b-s-ad <89061373+b-s-ad@users.noreply.github.com>
2022-01-13 17:23:50 -0800

    B

---
+++ B.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Alpha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            System.Console.WriteLine("\nAlphabet_pattern : B\n");
+            for (int i = 1; i <= 5; i++)
+            {
+                for (int j = 1; j <= 5; j++)
+                {
+                    string B = (i == 1 || i == 3 || i == 5 || j == 1 || j == 5) ? "*" : " ";
+                    System.Console.Write(B);
+                }
+                System.Console.WriteLine();
+            }
+            /* ***************************************************** */
+        }
+    }
+}
