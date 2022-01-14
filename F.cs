e92a8e44a9f2f71e58dc4b10c5cc41a0205376e9
b-s-ad <89061373+b-s-ad@users.noreply.github.com>
2022-01-13 17:30:12 -0800

    F

---
+++ F.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Alpha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            System.Console.WriteLine("\nAlphabet_pattern : F\n");
+            for (int i = 1; i <= 5; i++)
+            {
+                for (int j = 1; j <= 5; j++)
+                {
+                    string F = (i == 3 || i == 1 || j == 1 || j == 0) ? "*" : " ";
+                    System.Console.Write(F);
+                }
+                System.Console.WriteLine();
+            }
+            /* ***************************************************** */
+        }
+    }
+}
