d570ef3f9ca08ed84882ce6d88d076b66b2a1a77
b-s-ad <89061373+b-s-ad@users.noreply.github.com>
2022-01-13 18:26:41 -0800

    O <r>ZERO

---
+++ O Zero.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Alpha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            System.Console.WriteLine("\nAlphabet_pattern : O\n");
+            for (int i = 1; i <= 6; i++)
+            {
+                for (int j = 1; j <= 6; j++)
+                {
+                    string O = (i == 1 || j == 1 || j == 6 || i == 6) ? "*" : " ";
+                    System.Console.Write(O);
+                }
+                System.Console.WriteLine();
+            }
+            /* ***************************************************** */
+        }
+    }
+}
+
