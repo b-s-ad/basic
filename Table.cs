8439c460a55fe98a856be927f8530fe459b569ff
b-s-ad <89061373+b-s-ad@users.noreply.github.com>
2022-01-13 18:07:52 -0800

    Table

---
+++ Table.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Alpha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            System.Console.WriteLine("\nAlphabet_pattern : Table \n");
+            for (int i = 1; i <= 5; i++)
+            {
+                for (int j = 1; j <= 9; j++)
+                {
+                    string Table = (i == 1 || i + j == 6 || j - i == 4) ? "*" : " ";
+
+                    System.Console.Write(Table);
+                }
+                System.Console.WriteLine();
+            }
+            /* ***************************************************** */
+        }
+    }
+}
+
