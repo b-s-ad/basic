e72dcecb6fd9ed88fecfbdc7830e662333d19198
b-s-ad <89061373+b-s-ad@users.noreply.github.com>
2022-01-13 18:28:40 -0800

    M

---
+++ M.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Alpha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            System.Console.WriteLine("\nAlphabet_pattern : M\n");
+            for (int i = 1; i <= 5; i++)
+            {
+                for (int j = 1; j <= 5; j++)
+                {
+                    string M = (j == 1 || j == 5 || i == 2 && j >= 2 && j <= 2 || i == 3 && j >= 3 && j <= 3 || i == 2 && j >= 4 && j <= 4) ? "*" : " ";
+                    System.Console.Write(M);
+                }
+                System.Console.WriteLine();
+            }
+            /* ***************************************************** */
+        }
+    }
+}
+
