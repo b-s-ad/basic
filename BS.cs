0b20a78fa43594ed1a57f6fc17674a91825708b9
b-s-ad <89061373+b-s-ad@users.noreply.github.com>
2022-01-14 10:00:24 -0800

    Bubble Sort Int[]Array

---
+++ BS.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace bs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            int[] arr = { 30, 20, 19, 13, 3, 52 };
+            int temp;
+            int n = arr.Length;
+            System.Console.WriteLine("Program Starts");
+            System.Console.WriteLine("\nbefore Sorting");
+
+            foreach (var item in arr)
+            {
+                System.Console.Write(" " + item);
+                //Console.ReadLine();                
+            }
+            for (int i = 0; i < n - 1; i++)
+            {
+                for (int j = 0; j < n - 1; j++)
+                {
+                    if (arr[j] > arr[j + 1])
+                    {
+                        temp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp;
+
+                    }
+                }
+            }
+            System.Console.WriteLine("\nSorted : \n");
+            foreach (var item in arr)
+            {
+                Console.Write(item + " ");
+                //Console.Read();
+            }
+        }
+
+    }
+}
+
+
