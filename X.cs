e193170c1169539232c9e572dfb8cbd5df31b6bf
b-s-ad <89061373+b-s-ad@users.noreply.github.com>
2022-01-13 17:22:25 -0800

    X
    X

---
+++ X.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Alpha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            System.Console.WriteLine("\nAlphabet_pattern : X\n");
+            for (int i = 1; i <= 5; i++)
+            {
+                for (int j = 1; j <= 5; j++)
+                {
+                    string X = (i + j == 6 || j - i == 0) ? "*" : " ";
+                    System.Console.Write(X);
+                }
+                System.Console.WriteLine();
+            }
+            /* ***************************************************** */
+        }
+    }
+}
